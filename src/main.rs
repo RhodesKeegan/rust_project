7c491aa3ff4d1dde2e1d91a5343b2be45db8fabe
RhodesKeegan <rhodeskeegan@mailfence.com>
2025-07-25 10:40:26 -0500

    adding rust examples

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,63 @@
-// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::error::Error;
+const fn add(x:u32, y:u32) -> u32
+{
+    x+y
+}
 
-slint::include_modules!();
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let ui = AppWindow::new()?;
+struct Number
+{
+    value: i32
+}
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+const fn add_numbers() -> i32
+{
+    let x = 12;
+    let number = if x < 11 {5} else {6};
+    let mut counter = 0;
+    let result = loop {
+        counter += number;
+        if counter > 3*x
+        {
+            break counter*x;
         }
-    });
+    };
+    result
+}
 
-    ui.run()?;
+fn main()
+{
 
-    Ok(())
+    let a: f32 = 5.3;
+    let b: f64 = 10.5;
+
+
+    let mut result: u32 = add(a as u32, b as u32);
+    result+=10;
+
+    println!("{}",result);
+
+
+
+
+    let mut string: String = String::from("hello");
+    let first_ref = &string;
+    let second_ref = &string;
+
+    println!("{}, {}", first_ref, second_ref);
+
+    let third_ref = &mut string;
+    *third_ref += ", world!";
+
+    println!("{}", third_ref);
+
+
+
+
+    let number = Number{value: 5};
+    let b = number;
+
+    println!("The result is {}", b.value);
+
+    println!("The result of add number is {}", add_numbers());
 }
